d5ab9934a43500f1e00383bbeed2e90493819c3a
MasterOfCubesAU <masterofcubesau@gmail.com>
2023-01-05 05:51:56 +1100

    Added generic on_error handler

--- lib/cogs/ErrorHandler.py
+++ lib/cogs/ErrorHandler.py
@@ -15,17 +15,22 @@ class ErrorHandler(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.bot.tree.on_error = self.on_app_command_error
+        self.bot.on_error = self.on_error
         self.logger = logging.getLogger(__name__)
 
     async def cog_load(self):
         self.logger.info(f"[COG] Loaded {self.__class__.__name__}")
         
     async def on_app_command_error(self, interaction, error):
-        embed = self.bot.create_embed("SCUFFBOT ERROR", "An unexpected error has occurred.", 0xFF0000)
+        embed = self.bot.create_embed("An unexpected error has occurred." , title="SCUFFBOT ERROR", colour=0xFF0000)
         embed.add_field(name="ERROR:", value="> {}\n\nIf this error is a regular occurrence, please contact {}. This error has been logged.".format(str(error), self.appinfo.owner.mention), inline=False)
         await interaction.response.send_message(embed=embed, ephemeral=True)
         
-        self.logger.error(f"[ERROR] Unhandled Error: {error}")
+        self.logger.error(f"[ERROR] App Command Error: {error}")
+        traceback.print_exc()
+    
+    async def on_error(self, event):       
+        self.logger.error(f"[ERROR] Unhandled Error: {event}")
         traceback.print_exc()
 
 
